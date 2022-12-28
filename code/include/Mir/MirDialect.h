38b497fe4ccb6ccfce84a8847c9a8dc5e7f63d1e
Thorsten Schütt <schuett@gmail.com>
2022-12-28 16:24:16 +0100

    dialect

--- code/include/Mir/MirDialect.h
+++ code/include/Mir/MirDialect.h
@@ -1,13 +1,12 @@
 #pragma once
 
-#include "mlir/IR/BuiltinTypes.h"
-#include "mlir/IR/Dialect.h"
-#include "mlir/IR/FunctionInterfaces.h"
-#include "mlir/IR/OpDeqfinition.h"
-#include "mlir/IR/SymbolTable.h"
-#include "mlir/Interfaces/CallInterfaces.h"
-#include "mlir/Interfaces/CastInterfaces.h"
-#include "mlir/Interfaces/SideEffectInterfaces.h"
+#include <mlir/IR/BuiltinTypes.h>
+#include <mlir/IR/Dialect.h>
+#include <mlir/IR/FunctionInterfaces.h>
+#include <mlir/IR/SymbolTable.h>
+#include <mlir/Interfaces/CallInterfaces.h>
+#include <mlir/Interfaces/CastInterfaces.h>
+#include <mlir/Interfaces/SideEffectInterfaces.h>
 
 #include "MirDialect.h.inc"
 
