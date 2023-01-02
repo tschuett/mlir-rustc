9113e5ba630b1790925310ddf66f861751187b10
Thorsten Schütt <schuett@gmail.com>
2023-01-02 11:54:04 +0100

    codegen MLIR

--- code/moduleBuilder/OperatorExpression.cpp
+++ code/moduleBuilder/OperatorExpression.cpp
@@ -25,17 +25,15 @@ mlir::Value ModuleBuilder::emitArithmeticOrLogicalExpression(
   switch (expr->getKind()) {
   case ast::ArithmeticOrLogicalExpressionKind::Addition: {
     return builder.create<Mir::AddOp>(location, lhs, rhs);
-    break;
   }
   case ast::ArithmeticOrLogicalExpressionKind::Subtraction: {
     return builder.create<Mir::SubOp>(location, lhs, rhs);
-    break;
   }
   case ast::ArithmeticOrLogicalExpressionKind::Multiplication: {
     return builder.create<Mir::MulOp>(location, lhs, rhs);
-    break;
   }
   case ast::ArithmeticOrLogicalExpressionKind::Division: {
+    return builder.create<Mir::DivOp>(location, lhs, rhs);
     break;
   }
   case ast::ArithmeticOrLogicalExpressionKind::Remainder: {
