aa1971a305f540a4fc327a1e0d54f9206c1ec08b
Thorsten Schütt <schuett@gmail.com>
2023-01-07 12:08:38 +0100

    expression parsing

--- code/parser/Expression.cpp
+++ code/parser/Expression.cpp
@@ -6,9 +6,21 @@ using namespace rust_compiler::lexer;
 namespace rust_compiler::parser {
 
 std::optional<std::shared_ptr<ast::Expression>>
-tryParseExpression(std::span<lexer::Token>) {
+tryParseExpression(std::span<lexer::Token> tokens) {
+  std::span<lexer::Token> view = tokens;
 
-  // FIXME
+  std::optional<std::shared_ptr<ast::Expression>> woBlock =
+      tryParseExpressionWithoutBlock(view);
+  if (woBlock) {
+    return woBlock;
+  }
+
+  std::optional<std::shared_ptr<ast::Expression>> withBlock =
+      tryParseExpressionWithBlock(view);
+  if (withBlock) {
+    return withBlock;
+  }
+
   return std::nullopt;
 }
 
