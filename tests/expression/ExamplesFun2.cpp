4d8a120121fa87a90679b4b2e526ae813322323d
Thorsten Schütt <schuett@gmail.com>
2023-01-19 21:43:51 +0100

    extend test coverage; bisecting

--- tests/expression/ExamplesFun2.cpp
+++ tests/expression/ExamplesFun2.cpp
@@ -19,7 +19,7 @@ using namespace rust_compiler::ast;
 TEST(ExamplesFun2Test, CheckFun1) {
 
   std::string text =
-      "pub fn add(right: usize) -> usize {  return if true { 5 } else { 6 } };";
+      "pub fn add(right: usize) -> usize {  return if true { 5 } else { 6 } }";
 
   TokenStream ts = lex(text, "lib.rs");
 
@@ -35,6 +35,66 @@ TEST(ExamplesFun2Test, CheckFun2) {
 
   std::string text =
       "fn add(right: usize) -> usize { return if true { 5 } else { 6 } }";
+
+  TokenStream ts = lex(text, "lib.rs");
+
+  Parser parser = {ts, ""};
+
+  std::optional<rust_compiler::ast::Function> fun =
+      parser.tryParseFunction(ts.getAsView(), "");
+
+  EXPECT_TRUE(fun.has_value());
+};
+
+TEST(ExamplesFun2Test, CheckFun2a) {
+
+  std::string text =
+      "fn add(right: usize) -> usize { return if true { 5 } }";
+
+  TokenStream ts = lex(text, "lib.rs");
+
+  Parser parser = {ts, ""};
+
+  std::optional<rust_compiler::ast::Function> fun =
+      parser.tryParseFunction(ts.getAsView(), "");
+
+  EXPECT_TRUE(fun.has_value());
+};
+
+TEST(ExamplesFun2Test, CheckFun2ab) {
+
+  std::string text =
+      "return if true { 5 }";
+
+  TokenStream ts = lex(text, "lib.rs");
+
+  Parser parser = {ts, ""};
+
+  std::optional<std::shared_ptr<rust_compiler::ast::Expression>> fun =
+      parser.tryParseReturnExpression(ts.getAsView());
+
+  EXPECT_TRUE(fun.has_value());
+};
+
+TEST(ExamplesFun2Test, CheckFun2ac) {
+
+  std::string text =
+      "return if true { 5 } else { 6 }";
+
+  TokenStream ts = lex(text, "lib.rs");
+
+  Parser parser = {ts, ""};
+
+  std::optional<std::shared_ptr<rust_compiler::ast::Expression>> fun =
+      parser.tryParseReturnExpression(ts.getAsView());
+
+  EXPECT_TRUE(fun.has_value());
+};
+
+TEST(ExamplesFun2Test, CheckFun2b) {
+
+  std::string text =
+      "fn add(right: usize) -> usize {  return 5; };";
 
   TokenStream ts = lex(text, "lib.rs");
 
--- tests/expression/IfExpression.cpp
+++ tests/expression/IfExpression.cpp
@@ -31,7 +31,7 @@ TEST(IfExpressionTest, CheckIfExpression) {
 
 TEST(IfExpressionTest, CheckIfExpression1) {
 
-  std::string text = "if 5 { 4 } else { 5}";
+  std::string text = "if 5 { 4 } else { 5 }";
 
   TokenStream ts = lex(text, "lib.rs");
 
@@ -58,6 +58,46 @@ TEST(IfExpressionTest, CheckIfExpression2) {
   printTokenState(ts.getAsView());
 
   size_t expectedLendth = 11;
+
+  EXPECT_EQ(ts.getLength(), expectedLendth);
+
+  Parser parser = {ts, ""};
+
+  std::optional<std::shared_ptr<rust_compiler::ast::Expression>> ifExpr =
+      parser.tryParseIfExpression(ts.getAsView());
+
+  EXPECT_TRUE(ifExpr.has_value());
+};
+
+TEST(IfExpressionTest, CheckIfExpression3) {
+
+  std::string text = "if 5 { 4 } else if true { 5 } else { 6 }";
+
+  TokenStream ts = lex(text, "lib.rs");
+
+  printTokenState(ts.getAsView());
+
+  size_t expectedLendth = 15;
+
+  EXPECT_EQ(ts.getLength(), expectedLendth);
+
+  Parser parser = {ts, ""};
+
+  std::optional<std::shared_ptr<rust_compiler::ast::Expression>> ifExpr =
+      parser.tryParseIfExpression(ts.getAsView());
+
+  EXPECT_TRUE(ifExpr.has_value());
+};
+
+TEST(IfExpressionTest, CheckIfExpression4) {
+
+  std::string text = "if 5 { 4 } else if true { 5 } if true { 6 } else { 6 }";
+
+  TokenStream ts = lex(text, "lib.rs");
+
+  printTokenState(ts.getAsView());
+
+  size_t expectedLendth = 20;
 
   EXPECT_EQ(ts.getLength(), expectedLendth);
 
