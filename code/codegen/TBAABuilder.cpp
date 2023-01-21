fdb3398d49fb9270d793412e0cc5b4426d534439
Thorsten Schütt <schuett@gmail.com>
2023-01-21 16:46:49 +0100

    tbaa

--- code/codegen/TBAABuilder.cpp
+++ code/codegen/TBAABuilder.cpp
@@ -1,6 +1,28 @@
 #include "TBAABuilder.h"
 
+using namespace mlir;
+using namespace mlir::LLVM;
+
 namespace rust_compiler::codegen {
 
-
+std::string TBAABuilder::getNewTBAANodeName(llvm::StringRef basename) {
+  return (llvm::Twine(basename) + llvm::Twine('_') +
+          llvm::Twine(tbaaNodeCounter++))
+      .str();
 }
+
+TBAABuilder::TBAABuilder(mlir::ModuleOp module) {
+  // Create TBAA MetadataOp with the root and basic type descriptors.
+  Location loc = module.getLoc();
+  MLIRContext *context = module.getContext();
+  OpBuilder builder(module.getBody(), module.getBody()->end());
+  tbaaMetaOp = builder.create<MetadataOp>(loc, tbaaMetaOpName);
+  builder.setInsertionPointToStart(&tbaaMetaOp.getBody().front());
+
+  // Root node.
+  auto rootOp = builder.create<TBAARootMetadataOp>(
+      loc, getNewTBAANodeName(kRootSymBasename), rustcTBAARootId);
+  rustcTBAARoot = FlatSymbolRefAttr::get(rootOp);
+}
+
+} // namespace rust_compiler::codegen
--- code/codegen/TBAABuilder.h
+++ code/codegen/TBAABuilder.h
@@ -7,15 +7,32 @@ namespace rust_compiler::codegen {
 
 class TBAABuilder {
 public:
-  TBAABuilder(mlir::ModuleOp module, bool applyTBAA);
+  TBAABuilder(mlir::ModuleOp module);
   TBAABuilder(TBAABuilder const &) = delete;
   TBAABuilder &operator=(TBAABuilder const &) = delete;
 
 private:
+  // Return unique string name based on `basename`.
+  std::string getNewTBAANodeName(llvm::StringRef basename);
+
   // LLVM::MetadataOp holding the TBAA operations.
   mlir::LLVM::MetadataOp tbaaMetaOp;
   // Symbol name of tbaaMetaOp.
   static constexpr llvm::StringRef tbaaMetaOpName = "__rustc_tbaa";
+
+  // Base names for TBAA operations:
+  //   TBAARootMetadataOp:
+  static constexpr llvm::StringRef kRootSymBasename = "root";
+
+  // Symbol defined by the LLVM::TBAARootMetadataOp identifying
+  // Rustc's TBAA root.
+  mlir::SymbolRefAttr rustcTBAARoot;
+
+  // Identity string for rustc's TBAA root.
+  static constexpr llvm::StringRef rustcTBAARootId = "Rustc Type TBAA Root";
+
+  // Counter for unique naming of TBAA operations' symbols.
+  unsigned tbaaNodeCounter = 0;
 };
 
 } // namespace rust_compiler::codegen
