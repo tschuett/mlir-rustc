8445dddaa43dd11c6af6ee863861cbd6c19f856e
Thorsten Schütt <schuett@gmail.com>
2023-03-23 11:22:01 +0100

    cycles

--- code/analysis/Cycles.cpp
+++ code/analysis/Cycles.cpp
@@ -2,6 +2,23 @@
 
 namespace rust_compiler::analysis {
 
-void Cycles::analyze(mlir::func::FuncOp *f) {}
+void Cycles::depthFirstSearch(mlir::Block *block, uint32_t currentDepth) {
+  // visit current node
+  depths.insert({block, currentDepth});
+  // left sutree
+  // right tree
+  for (auto *b : block->getSuccessors())
+    depthFirstSearch(b, currentDepth + 1);
+}
+
+void Cycles::analyze(mlir::func::FuncOp *f) {
+  fun = f;
+
+  // first step: depth first search
+  depthFirstSearch(&f->getRegion().front(), 0);
+
+  for (unsigned i = 0; i < depths.size(); ++i) {
+  }
+}
 
 } // namespace rust_compiler::analysis
--- code/include/Analysis/Cycles.h
+++ code/include/Analysis/Cycles.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <llvm/ADT/MapVector.h>
 #include <llvm/ADT/SmallPtrSet.h>
 #include <mlir/Dialect/Func/IR/FuncOps.h>
 #include <mlir/IR/Block.h>
@@ -11,11 +12,23 @@ namespace rust_compiler::analysis {
 using namespace mlir;
 
 /// Nesting of Reducible and Irreducible Loops
-/// Paul Havlak
+/// Paul Havlak, 1997
 class Cycles {
 
 public:
   void analyze(mlir::func::FuncOp *f);
+
+private:
+  /// DFS in preorder
+  void depthFirstSearch(mlir::Block *block, uint32_t currentDepth);
+
+  /// dominator tree
+  mlir::DominanceInfo domInfo;
+
+  /// current function
+  mlir::func::FuncOp *fun;
+
+  llvm::SmallMapVector<mlir::Block *, uint32_t, 8> depths;
 };
 
 } // namespace rust_compiler::analysis
